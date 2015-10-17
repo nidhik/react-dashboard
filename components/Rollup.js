2f8f202fa9fce5f7b2a948766b85bf77159c7441
nidhik <nidhikulkarni82@gmail.com>
2015-10-16 20:30:47 -0700

    Add chartist-react and chartist css file. Now we have a static horizontal bar graph in roll up

--- components/Rollup.js
+++ components/Rollup.js
@@ -1,5 +1,23 @@
 var React = require('react');
 var Parse = require('parse');
+var ChartistGraph = require('react-chartist')
+
+var simpleChartData = {
+  labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+  series: [
+    [5, 4, 3, 7, 5, 10, 3],
+    [3, 2, 9, 5, 4, 6, 4]
+  ]
+}
+
+var myOptions = {
+  seriesBarDistance: 10,
+  reverseData: true,
+  horizontalBars: true,
+  axisY: {
+    offset: 70
+  }
+}
 
 
 var Rollup = React.createClass({
@@ -10,7 +28,11 @@ var Rollup = React.createClass({
 
             <div>
 
-            <div>Summary Graph</div>
+            <div>
+                <ChartistGraph data={simpleChartData} options = {myOptions} type={'Bar'} />
+               
+      
+            </div>
             <div>Summary Letter Progress</div>
             <div>Summary Word Progress</div>
 
