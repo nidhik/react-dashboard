fb8a87798f43626eee963b5d6343849719c869aa
nidhik <nidhikulkarni82@gmail.com>
2016-01-03 21:30:39 -0800

    display table of words by score

--- components/Words.js
+++ components/Words.js
@@ -4,6 +4,48 @@ var ParseReact = require('parse-react');
 var ParseCloudCodeMixin = require('./ParseCloudCodeMixin');
 
 
+var WordRow = React.createClass({
+    render: function() {
+    
+        return (
+            <tr> 
+                <td>{ this.props.word.get("chars") }</td>
+                <WordScore score = { this.props.score }  />
+            </tr>
+        );
+    }
+});
+
+var WordScore = React.createClass({
+    render: function() {
+    
+        return (
+            <td>{ this.props.score ? this.props.score.get('averageScore') : "n/a" }</td>
+        );
+    }
+});
+
+var WordTable = React.createClass({
+    render: function() {
+
+        var rows = [];
+        var that = this;
+        this.props.words.forEach(function(word) {
+           rows.push(<WordRow word = { word } score = { that.props.scores[ word.id ] } key = { word.id } />);
+        });
+
+
+        return (
+            <div>
+            <table>
+                <tbody>{ rows }</tbody>
+            </table>
+            </div>
+        );
+    }
+});
+
+
 var Words = React.createClass({
 
     mixins: [ ParseCloudCodeMixin],
@@ -27,26 +69,19 @@ var Words = React.createClass({
       }
     },
 
-    render (){
-        if (this.data.words) {
+    render () {
+        if (this.data.words && this.data.scores) {
 
-            console.log(this.data.words.get('order'));
             return (
 
             <div>
 
             <h1>Detailed Word Proficency</h1>
-            <h2> Number of students: {this.data.students.length} </h2>
+            <h2> Number of students: { this.data.students.length } </h2>
             <h2> Number of words: { this.data.words.get('order').length } </h2> 
 
-            <ul>
-
-                {   this.data.words.get('order').map(function(w) {
-                        return <li>{ w.get('chars') }</li>;
-                    })
-                }
-
-            </ul>
+            <WordTable words = { this.data.words.get('order') } scores = { this.data.scores } />
+            
 
             </div>
 
@@ -56,10 +91,9 @@ var Words = React.createClass({
         return (
 
             <div>
-
             <h1>Detailed Word Proficency</h1>
-             <span>Loading Students...</span> 
-             <span>Loading Words...</span> 
+            <span>Loading Students...</span> 
+            <span>Loading Words...</span> 
             </div>
 
             );
